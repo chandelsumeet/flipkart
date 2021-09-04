4f841010d6b9e69d0940fde9951c7f25a91341a7
sumeet <sumeetchandel321@gmail.com>
2021-09-04 11:00:14 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+console.log("hello world");
+const crousel = document.getElementById("crousel-images");
+const prev = document.getElementById("prev-btn");
+const next = document.getElementById("next-btn");
+
+const img = document.querySelectorAll("#crousel-images img");
+let imgWidth = img[0].clientWidth;
+console.log(imgWidth);
+let idx = 0;
+let run = () => {
+  idx++;
+
+  changeImage();
+};
+
+let interval = setInterval(run, 2000);
+
+let changeImage = () => {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  crousel.style.transform = `translateX(${-idx * 1519}px)`;
+};
+
+let resetInterval = () => {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+};
+
+next.addEventListener("click", () => {
+  console.log("clicked");
+  idx++;
+  changeImage();
+  resetInterval();
+});
+
+prev.addEventListener("click", () => {
+  console.log("clicked");
+  idx--;
+  changeImage();
+  resetInterval();
+});
