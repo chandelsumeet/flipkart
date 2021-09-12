19d0bc70283c205afcc98911ffd9bdbeb327d0f1
sumeet <sumeetchandel321@gmail.com>
2021-09-12 12:40:46 +0530

    Added dropdown menus

--- scripts/app.js
+++ scripts/app.js
@@ -1,61 +1,70 @@
-console.log("hello world");
-const crousel = document.getElementById("crousel-images");
-const prev = document.getElementById("prev-btn");
-const next = document.getElementById("next-btn");
+console.log("hello world")
+const crousel = document.getElementById("crousel-images")
+const prev = document.getElementById("prev-btn")
+const next = document.getElementById("next-btn")
 
-const img = document.querySelectorAll("#crousel-images img");
-let imgWidth = img[0].clientWidth;
-console.log(imgWidth);
-let idx = 0;
+const img = document.querySelectorAll("#crousel-images img")
+let imgWidth = img[0].clientWidth
+console.log(imgWidth)
+let idx = 0
 let run = () => {
-  idx++;
+  idx++
 
-  changeImage();
-};
+  changeImage()
+}
 
-let interval = setInterval(run, 2000);
+let interval = setInterval(run, 3000)
 
 let changeImage = () => {
   if (idx > img.length - 1) {
-    idx = 0;
+    idx = 0
   } else if (idx < 0) {
-    idx = img.length - 1;
+    idx = img.length - 1
   }
 
-  crousel.style.transform = `translateX(${-idx * 1519}px)`;
-};
+  crousel.style.transform = `translateX(${-idx * 1519}px)`
+}
 
 let resetInterval = () => {
-  clearInterval(interval);
-  interval = setInterval(run, 2000);
-};
+  clearInterval(interval)
+  interval = setInterval(run, 2000)
+}
 
 next.addEventListener("click", () => {
-  console.log("clicked");
-  idx++;
-  changeImage();
-  resetInterval();
-});
+  console.log("clicked")
+  idx++
+  changeImage()
+  resetInterval()
+})
 
 prev.addEventListener("click", () => {
-  console.log("clicked");
-  idx--;
-  changeImage();
-  resetInterval();
-});
+  console.log("clicked")
+  idx--
+  changeImage()
+  resetInterval()
+})
 
 // modal script
-let loginBtn = document.getElementById("login-btn");
-let modal = document.getElementById("modal-container");
+let loginBtn = document.getElementById("login-btn")
+let modal = document.getElementById("modal-container")
 const outside = (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    closeModal()
   }
-};
-document.addEventListener("click", outside);
+}
+document.addEventListener("click", outside)
 
 loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("clicked");
-  modal.style.display = "block";
-});
+  e.preventDefault()
+  console.log("clicked")
+  modal.style.display = "block"
+  document.body.style.overflow = "hidden"
+})
+
+const closeBtn = document.querySelector(".modal-close")
+closeBtn.addEventListener("click", closeModal)
+
+function closeModal() {
+  modal.style.display = "none"
+  document.body.style.overflow = "scroll"
+}
